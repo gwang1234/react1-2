3475ed5c02775fb3386c967616a24c5449416388
KimMinJeong <qoi11@naver.com>
2024-03-27 16:42:22 +0900

    3월 27일

---
+++ test/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import reportWebVitals from './reportWebVitals';
+import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+import Hello from './chapter_03/Hello';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// setInterval(() => {
+//   root.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>
+//   );
+// })
+
+root.render(
+  <React.StrictMode>
+    <Hello />
+  </React.StrictMode>
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
